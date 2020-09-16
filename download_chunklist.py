026fab78fd1dc10b5de77db86e1ccd88ed4193fc
HyoungjooChae <mapoout@naver.com>
2020-09-16 12:18:01 +0900

    ㅇ

--- download_chunklist.py
+++ download_chunklist.py
@@ -16,7 +16,3 @@ def get_chunklist(url):
 
 
 
-
-
-if __name__ == '__main__':
-    get_chunklist("https://cdn.yonsei.ac.kr/yonsei/_definst_/mp4:yscecictl_yscec/410493/1600164565/%ED%99%95%EB%A5%A0%ED%86%B5%EA%B3%84(3.4(2)).mp4/chunklist.m3u8")
