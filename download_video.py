d47fe1d37610e08f0b30e8ee03c5257644a46952
HyoungjooChae <mapoout@naver.com>
2020-09-16 16:00:04 +0900

    Update download_video.py

--- download_video.py
+++ download_video.py
@@ -1,6 +1,8 @@
 import requests
 
 from download_chunklist import get_chunklist
+
+
 BASE_URL = input("가져온 URL에서 /media_**.ts 부분을 제외하고 입력해 주세요 : ")
 OUTPUT_FILE_NAME = input("생성할 비디오 파일명을 입력해 주세요 (ex. yonsei.mp4) : ")
 chunk_size = 256
